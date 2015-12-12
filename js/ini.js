fe3b8d007c773d64bc57d4869c9f1aabdfc359bf
perthblank <496267655@qq.com>
2015-12-12 09:07:45 +0800

    update

--- js/ini.js
+++ js/ini.js
@@ -1,9 +1,14 @@
 $(document).ready(init);
 
 function init(){
-	$(".divCard").click(function(){
+	$(".divCard").click(function(ev){
 		var url = $(this).children("a").attr("href");
-		location.href=url;
+		//location.href=url;
+
+		ev.preventDefault();
+		$("#site_content").fadeOut();
+		$("#dvHead").hide();
+		$("#site_content2").load(url)
 			
 			});
 }
