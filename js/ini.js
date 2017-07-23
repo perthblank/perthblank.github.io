d09f683cc527866eed6dba4d8407f733ac435bba
corey <corey.hdu@gmail.com>
2017-07-23 13:07:37 -0700

    update

--- js/ini.js
+++ js/ini.js
@@ -4,52 +4,38 @@ var strSeeCBlog = "The code is been migrating, check it on my Chinese blog (Just
 
 function loadContent(url){
 
-	$("#site_content").fadeOut();
-	$("#dvHead").hide();
-	$("#site_content2_in").load(url);
-	$("#site_content2").show();
-	
-	window.scroll(0,0);	
+  $("#site_content").fadeOut();
+  $("#dvHead").hide();
+  $("#site_content2_in").load(url);
+  $("#site_content2").show();
+  
+  window.scroll(0,0);  
 }
 
 function init(){
 
-	$("#site_content2").hide();
-	$(".divCard").click(function(ev){
-		ev.preventDefault();
-		var link = $(this).children("a");
-		var url = $(this).attr("targ");
+  $("#site_content2").hide();
+  $(".divCard").on("click", function(ev){
+    ev.preventDefault();
+    var link = $(this).children("a");
+    var url = $(this).attr("targ");
 
-		if($(this).hasClass("inPageLink")){
-			location.href="post.html?targ="+url;
-		}
+    if($(this).hasClass("inPageLink")){
+      location.href="post.html?targ="+url;
+    }
 
-		else{
-			location.href=url;
-		}
-	
-	});
+    else{
+      location.href=url;
+    }
+  
+  });
 
-	$(".inPageLinkCont").click(function(ev){
-		ev.preventDefault();
-		var link = $(this).children("a");
-		var url = link.attr("href");
-		loadContent(url);
-	});
-
-
-	$("#lava_menu li").click(function(){
-		$("#lava_menu .current").removeClass("current");		
-	
-	});
-
-	$("#btn_project").click(function(){
-		$("#site_content2").hide();
-		$("#dvHead").show();
-		$("#site_content").show();
-	
-	});
-
+  $(".inPageLinkCont").on("click", function(ev){
+    ev.preventDefault();
+    var link = $(this).children("a");
+    var url = link.attr("href");
+    loadContent(url);
+  });
 
 }
 
